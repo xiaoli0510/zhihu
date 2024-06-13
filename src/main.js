8d7d4c8ab4fcaf7a8735b7cbe635526a7a51c4cd
xiaoxiao <253451518@qq.com>
2024-06-13 20:36:08 +0800

    使用Pinia写todo浮窗组件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import {createPinia } from 'pinia';
-
-// import store from './store/index.js'
 import 'amfe-flexible';
 import '@/mock/index'
 import '@/mock/search.js'
