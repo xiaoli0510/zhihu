bf1dfc62b2a55532059079e60ce8b1fcf819c1d1
xiaoxiao <253451518@qq.com>
2024-06-17 21:21:01 +0800

    目录组件

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,21 +1,55 @@
 <script setup>
 import { ref } from 'vue';
 import CatalogItem from './CatalogItem.vue'
+import BookIcon from './BookIcon.vue'
 const isShow = ref(true);
-    
+
 </script>
 <template>
-<van-popup
-  v-model:show="isShow"
-  round
-  position="bottom"
-  :style="{ height: '90%',padding:'10px' }"
->
-<h3>目录</h3>
-<p>根据您的兴趣，为您推荐的专属内容</p>
-<CatalogItem />
+  <van-popup v-model:show="isShow" round position="bottom" :style="{ height: '90%', padding: '10px' }">
+    <h2>目录</h2>
+    <van-row justify="space-around" class="brief">
+      <van-col span="7">
+        <van-image width="100" height="100" radius="5px" src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" />
+      </van-col>
+      <van-col class="txt" span="17" justify="space-around" align="bottom">
+        <h3>这是标题12</h3>
+        <van-row justify="space-between">
+          <van-col class="grey-font">查看详情<van-icon name="arrow" /></van-col>
+          <van-col span="9">
+            <BookIcon />
+          </van-col>
+        </van-row>
+      </van-col>
+    </van-row>
+    <van-row justify="space-between" class="total-sort">
+      <van-col class="grey-font">共222节·已完结</van-col>
+      <van-col class="grey-font"><van-icon name="exchange" />正序</van-col>
+    </van-row>
+    <div class="catalog-list">
+      <CatalogItem />
+    </div>
 
-</van-popup>
+  </van-popup>
 </template>
 <style scoped lang='scss'>
+.brief {
+  margin: 10px 0;
+
+  .txt {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+}
+
+.total-sort {
+  margin: 20px 0;
+}
+
+.grey-font {
+  color: #3d3d3d;
+  font-size: 12px;
+  border: 1px solid red;
+}
 </style>
--- src/components/CatalogItem.vue
+++ src/components/CatalogItem.vue
@@ -19,11 +19,13 @@ import { ref } from 'vue';
   <p>{{ item.sentence }}</p>
   <van-row justify="space-around">
     <van-col span="20">
-      <span v-for="item1 in item.feature">{{ item1 }}.</span>
+      <span v-for="item1 in item.feature">{{ item1 }}·</span>
       <span>{{ item.totalTxt }}</span>
     </van-col>
     <van-col span="4"></van-col>
   </van-row>
+  <van-divider />
+
  </div>
 </template>
 <style scoped lang='scss'>
