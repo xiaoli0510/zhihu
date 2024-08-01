789dd8e8b34ba7e9c7fcca1f52a0022bb94784e0
xiaoxiao <253451518@qq.com>
2024-08-01 13:00:17 +0800

    subject的discuss

--- src/views/home/subject.vue
+++ src/views/home/subject.vue
@@ -1,10 +1,71 @@
 <script setup>
+import BackIcon from '@/components/BackIcon.vue'
+import FollowIcon from '@/components/FollowIcon.vue'
+import ShareIcon from '@/components/BookShare/ShareIcon.vue'
+import { provide, ref } from 'vue';
+import SubjectDiscuss from './subject/Discuss.vue'
+
 const props = defineProps(['keyWord']);
 console.log(props.keyWord);
-    
+
+const item = ref({
+    id: 1,
+    name: '张三',
+    avatar: 'https://img.yzcdn.cn/vant/cat.jpeg',
+    isFollow: true,
+})
+const toggleFollow = (item) => {
+    item.isFollow = !item.isFollow;
+}
+provide('toggleFollow', toggleFollow);
+
+const activeName = ref(0);
+const tabLabel = ref({
+    0: '讨论',
+    1: '想法',
+    2: '待回答',
+})
+
 </script>
 <template>
-    关键字的讨论 想法 待回答
+    <div class="subject-wrap">
+        <div class="top">
+            <van-row justify="space-between" align="center">
+                <van-col>
+                    <BackIcon />
+                </van-col>
+                <van-col span="6">
+                    <FollowIcon :item="item" />
+                    <ShareIcon :data="{ iconName: 'ellipsis', iconSize: '25px' }" />
+                </van-col>
+            </van-row>
+            <h3 class="title-word"><van-icon name="chat" color="#1989fa" />{{ props.keyWord }}</h3>
+            <p class="gray-font detail">
+                1亿浏览.7.1万讨论
+            </p>
+        </div>
+
+        <van-tabs v-model:active="activeName" shrink class="tab-wrap">
+            <van-tab v-for="(item,index) in tabLabel" :title="item" :name="index">
+                内容 {{ index }}
+                <SubjectDiscuss />
+            </van-tab>
+        </van-tabs>
+    </div>
 </template>
 <style scoped lang='scss'>
+.subject-wrap {
+    background: #f7f7f7;
+
+    .top {
+        width: 95%;
+        margin: 0 auto;
+        .title-word{
+            margin:2% 0 1% 0;
+        }
+    }
+    .tab-wrap{
+        border-radius: 10px 10px 0 0;
+    }
+}
 </style>
---
+++ src/views/home/subject/Discuss.vue
@@ -0,0 +1,86 @@
+<script setup>
+
+</script>
+<template>
+
+    <van-row justify="space-between" class="gray-font sort">
+        <van-col>
+            全部内容
+        </van-col>
+        <van-col class="sort-wrap">
+            <span class="active">精华</span>
+            <span>|</span>
+            <span>最热</span>
+            <span>|</span>
+            <span>最新</span>
+        </van-col>
+    </van-row>
+
+    <div class="subject-discuss-list">
+        <div class="subject-discuss-item">
+            <van-row justify="start" align="center">
+                <van-col>
+                    <van-image round width="22px" height="22px"
+                        src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" />
+                </van-col>
+                <van-col offset="1">
+                    <span class="name">张三</span>
+                </van-col>
+            </van-row>
+            <div class="img-list">
+                <van-image width="5rem" height="2rem" radius="4px" class="img-detail"
+                    src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" />
+                <van-image width="5rem" height="2rem" radius="4px" class="img-detail"
+                    src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" />
+                <van-image width="5rem" height="2rem" radius="4px" class="img-detail"
+                    src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" />
+                <van-image width="5rem" height="2rem" radius="4px" class="img-detail"
+                    src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" />
+            </div>
+            <h3>zheshizeger</h3>
+            <p class="sentence">22222222</p>
+        </div>
+    </div>
+</template>
+<style scoped lang='scss'>
+.sort {
+    width: 97%;
+    margin: 2% 0;
+    .sort-wrap {
+        span {
+            margin: 0 2px;
+
+            &.active {
+                color: #000;
+            }
+        }
+    }
+}
+
+
+.subject-discuss-item {
+    width: 97%;
+    margin: 2% 0;
+    border: 1px solid red;
+
+    .name {
+        font-size: 10px;
+    }
+
+    .img-list {
+        width: 100%;
+        white-space: nowrap;
+        border: 1px soli d #000;
+        overflow: hidden;
+        overflow-x: scroll;
+
+        .img-detail {
+            margin: 0 1%;
+        }
+    }
+
+    .sentence {
+        color: #666666;
+    }
+}
+</style>
